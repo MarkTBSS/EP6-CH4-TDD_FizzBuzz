5eab702fd6b3990e524584014851dbeaa9fbfbbf
MARK\pirat <marktbss@gmail.com>
2024-03-22 09:40:27 +0700

    [GREEN] Input 11 - 15 Output Test Passed

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func FizzBuzz(nunber int) string {
+	if nunber == 15 {
+		return "FizzBuzz"
+	}
 	if nunber == 5 || nunber == 10 {
 		return "Buzz"
 	}
-	if nunber == 3 || nunber == 6 || nunber == 9 {
+	if nunber == 3 || nunber == 6 || nunber == 9 || nunber == 12 {
 		return "Fizz"
 	}
 	return fmt.Sprintf("%d", nunber)
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -21,6 +21,11 @@ func TestFizzBuzz(t *testing.T) {
 	testcase_8 := Case{name: "FizzBuzz(8)", number: 8, want: "8"}
 	testcase_9 := Case{name: "FizzBuzz(9)", number: 9, want: "Fizz"}
 	testcase_10 := Case{name: "FizzBuzz(10)", number: 10, want: "Buzz"}
+	testcase_11 := Case{name: "FizzBuzz(11)", number: 11, want: "11"}
+	testcase_12 := Case{name: "FizzBuzz(12)", number: 12, want: "Fizz"}
+	testcase_13 := Case{name: "FizzBuzz(13)", number: 13, want: "13"}
+	testcase_14 := Case{name: "FizzBuzz(14)", number: 14, want: "14"}
+	testcase_15 := Case{name: "FizzBuzz(15)", number: 15, want: "FizzBuzz"}
 	tests := []Case{
 		testcase_1,
 		testcase_2,
@@ -32,6 +37,11 @@ func TestFizzBuzz(t *testing.T) {
 		testcase_8,
 		testcase_9,
 		testcase_10,
+		testcase_11,
+		testcase_12,
+		testcase_13,
+		testcase_14,
+		testcase_15,
 	}
 
 	for _, testcase := range tests {
